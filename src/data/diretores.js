4562790d93e4b2f6e8dcb996d49f36c026a9aafd
brunovilareusebio <brunoveusebio@hotmail.com>
2025-07-17 20:59:38 -0300

    Create diretores.js

---
+++ src/data/diretores.js
@@ -0,0 +1,22 @@
+export const diretores = [
+  {
+    id: "wong-kar-wai",
+    nome: "Wong Kar-wai",
+    pais: "Hong Kong",
+    anosAtivos: "1988–presente",
+    estilo: ["neon", "emoção", "câmera lenta", "cor saturada", "intimidade urbana"],
+    filmesChave: ["in-the-mood-for-love", "chunking-express"],
+    relevancia: ["m2", "m8", "m13"],
+    leituras: ["Cinema e Cor", "Luz como Emoção"],
+  },
+  {
+    id: "terrence-malick",
+    nome: "Terrence Malick",
+    pais: "EUA",
+    anosAtivos: "1970–presente",
+    estilo: ["luz natural", "tempo", "intimidade", "paisagem", "narrativa não-linear"],
+    filmesChave: ["the-thin-red-line", "tree-of-life"],
+    relevancia: ["m2", "m4", "m9"],
+    leituras: ["O Tempo no Cinema", "Luz e Filosofia"],
+  },
+];
