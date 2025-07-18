aff331635a9e42aeacd0567e3983a83e5f903147
brunovilareusebio <brunoveusebio@hotmail.com>
2025-07-17 21:01:20 -0300

    Create eventos.js

---
+++ src/data/eventos.js
@@ -0,0 +1,18 @@
+export const eventos = [
+  {
+    id: "venice-film-festival",
+    nome: "Festival de Veneza",
+    data: "2025-08-25",
+    tipo: "festival",
+    local: "Veneza, Itália",
+    link: " https://la-biennale-venezia.it "
+  },
+  {
+    id: "sp-cine-club",
+    nome: "São Paulo Cine Club",
+    data: "2025-05-15",
+    tipo: "clube-de-exibicao",
+    local: "São Paulo, Brasil",
+    link: "/cultura/exibicao"
+  }
+];
