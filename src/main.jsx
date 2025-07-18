262a5881f33dc12b46202e66b9af68fe4308ad33
brunovilareusebio <brunoveusebio@hotmail.com>
2025-07-18 02:26:49 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { I18nProvider } from "./hooks/useTranslation";
 import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/my-film-revolution/">
+    <I18nProvider defaultLang="pt-BR">
+      <App />
+    </I18nProvider>
   </BrowserRouter>
 );
