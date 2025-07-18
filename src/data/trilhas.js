afc7b78b672bb5df15110efbd8aba2f06758e560
brunovilareusebio <brunoveusebio@hotmail.com>
2025-07-17 21:01:41 -0300

    Create trilhas.js

---
+++ src/data/trilhas.js
@@ -0,0 +1,16 @@
+export const trilhas = [
+  {
+    id: "trilha-cinema-brasileiro",
+    nome: "Cinema Brasileiro",
+    descricao: "Filmes, diretores e estética do cinema nacional.",
+    temas: ["brasileiro", "documentário", "poético"],
+    filmes: ["tangerine-2015", "nightcrawling-2014"],
+  },
+  {
+    id: "trilha-cidades-e-corpos",
+    nome: "Cidades e Corpos",
+    descricao: "Urbano, intimidade, som de ambiente.",
+    temas: ["urbano", "intimidade", "som ambiente"],
+    filmes: ["the-social-dilemma", "whiplash-2014"],
+  },
+];
