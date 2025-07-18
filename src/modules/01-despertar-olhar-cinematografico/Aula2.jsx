202038841d6ced86467bfb407490f9236b31928c
brunovilareusebio <brunoveusebio@hotmail.com>
2025-07-18 02:34:31 -0300

    Create Aula2.jsx

---
+++ src/modules/01-despertar-olhar-cinematografico/Aula2.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Aula2 = () => {
+  return (
+    <section>
+      <h2 className="text-xl font-semibold text-amber-400 mb-2">Aula 2: Observação Diária</h2>
+      <p className="mb-4">
+        Como observar o mundo ao seu redor e transformar em cinema.
+      </p>
+      <div className="bg-gray-800 p-4 rounded-lg">
+        <h3 className="font-semibold">Exercício:</h2>
+        <p>Grave 1 minuto de observação diária.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Aula2;
