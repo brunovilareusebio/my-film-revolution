50a33d9c0d653151687d9ebca2ffb9b63db3f8ce
brunovilareusebio <brunoveusebio@hotmail.com>
2025-07-17 21:13:52 -0300

    Create generate-sitemap.js

---
+++ src/scripts/src/scripts/generate-sitemap.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+const routes = [
+  "",
+  "modulos",
+  "modulos/despertar-olhar-cinematografico",
+  "modulos/luz-como-linguagem-emocional",
+  "modulos/design-grafico-para-audiovisual",
+  "biblioteca",
+  "comunidade",
+  "cultura",
+  "sobre",
+  "recursos",
+  "projeto",
+  "manifesto"
+];
+
+const domain = " https://seu-usuario.github.io/my-film-revolution ";
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${routes.map(route => `<url><loc>${domain}/${route}</loc><lastmod>${new Date().toISOString()}</lastmod></url>`).join("\n")}
+</urlset>`;
+
+fs.writeFileSync(path.join(__dirname, "..", "dist", "sitemap.xml"), sitemap, "utf8");
+console.log("✅ Sitemap gerado");
