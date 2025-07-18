210a8e551643a029abd38120de10ab4b273a7b03
brunovilareusebio <brunoveusebio@hotmail.com>
2025-07-18 02:35:02 -0300

    Create Aula3.jsx

---
+++ src/modules/01-despertar-olhar-cinematografico/Aula3.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Aula3 = () => {
+  return (
+    <section>
+      <h2 className="text-xl font-semibold text-amber-400 mb-2">Aula 3: Análise de Filmes</h2>
+      <p className="mb-4">Como analisar filmes e aprender com eles.</p>
+      <div className="bg-gray-800 p-4 rounded-lg">
+        <h3 className="font-semibold">Exercício:</h3>
