a25c184ab4edb7fe808b662c78bbaab174499271
brunovilareusebio <brunoveusebio@hotmail.com>
2025-07-18 23:22:55 -0300

    Create ModuleCard.jsx

---
+++ src/components/src/components/ModuleCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ModuleCard = ({ module }) => {
+  return (
+    <Link to={`/modulos/${module.slug}`} className="bg-gray-800 p-6 rounded-lg hover:shadow-amber-500/10 hover:shadow-lg transition">
+      <h3 className="text-xl font-semibold text-amber-500">{module.title}</h3>
+      <p className="text-sm text-gray-400">{module.subtitle}</p>
+    </Link>
+  );
+};
+
+export default ModuleCard;
